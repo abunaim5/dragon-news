e13a959c5765cbd77d75692f9908f6e701ac78fc
abunaim5 <abunaim140@gmail.com>
2024-04-06 16:50:02 +0600

    added navbar component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header/Header";
 import LatestNews from "../../components/LatestNews/LatestNews";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
     return (
@@ -7,7 +8,10 @@ const Home = () => {
             <div className="mb-8">
                 <Header></Header>
             </div>
+            <div className="mb-5">
             <LatestNews></LatestNews>
+            </div>
+            <NavBar></NavBar>
         </div>
     );
 };
